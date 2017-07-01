481cb1395c07c2c3213a6dc33cbe876d15ac2045
Dennis Lemon <dennis.lemon@dumpling-mining-company.de>
2017-07-01 11:34:56 +0200

    First version of the DMC Token

--- contracts/DMCToken.sol
+++ contracts/DMCToken.sol
@@ -1,15 +1,42 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.11;
 
-import "../zeppelin/token/ERC20.sol";
 
-// This is just a simple example of a coin-like contract.
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+import "../zeppelin/token/MintableToken.sol";
 
-contract DMCToken is ERC20 {
 
-	function DMCToken() {
-	}
+/**
+ * @title  DMC Token
+ * @dev The Dumpling Mining Company Token Smart Contract
+ */
 
+
+contract DMCToken is MintableToken {
+    string public constant name = "Dumpling Mining Company Token";
+    string public constant symbol = "DMC";
+    int8 public constant decimals = 18;
+
+    /**
+     * Construct the token.
+     * This token must be created through a Dumpling Mining Company UG multisig wallet, so that it is owned by that wallet.
+     * @param _initialSupply How many tokens we start with
+     */
+    function DMCToken(uint _initialSupply) {
+
+        if(0 == _initialSupply) {
+	    throw; // Without any supply it is useless to create a token
+        }
+
+        // Set the owner of this token
+        owner = msg.sender;
+
+        // Mint the tokens
+        if (false == mint(owner, _initialSupply)) {
+	    throw; // Minting of the tokens failed
+        }
+
+        // No more tokens can be minted after creation of the token. 
+        if (false == finishMinting()) {
+            throw; // Finish minting failed
+        }
+    }
 }
